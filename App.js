efb2b3773c5dcb3d88d7b063d335f3ac9a4b937f
janani-gopi <jananirave19@gmail.com>
2023-09-19 10:35:35 +0530

    feat:crop picker integration for camera and gallery:App.js:completed

--- App.js
+++ App.js
@@ -1,13 +1,60 @@
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import React, {useRef} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Image, Alert} from 'react-native';
+import React, {useRef, useState} from 'react';
 import Entypo from 'react-native-vector-icons/Entypo';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Feather from 'react-native-vector-icons/Feather';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import BottomSheet from 'react-native-gesture-bottom-sheet';
+import ImagePicker from 'react-native-image-crop-picker';
+
 const App = () => {
   const bottomSheet = useRef();
+  const [image, setImage] = useState([]);
 
+  //to open camera and upload image
+  function usingCamera() {
+    ImagePicker.openCamera({
+      width: 500,
+      height: 500,
+      cropping: true,
+    }).then(image => {
+      setImage(prev => [...prev, image]);
+    });
+  }
+  // open gallery and to upload image
+  function fromGallery() {
+    ImagePicker.openPicker({
+      width: 500,
+      height: 500,
+      cropping: true,
+    }).then(image => {
+      setImage(prev => [...prev, image]);
+    });
+  }
+  //to delete a single image
+  function deleteSingleImage(index) {
+    Alert.alert('Delete', 'You surely want to delete this image?', [
+      {
+        text: 'Cancel',
+        onPress: () => {
+          null
+        },
+        style: 'cancel',
+      },
+      {
+        text: 'OK',
+        onPress: () => {
+          const images = image.filter((each, index1) => {
+            if (index1 != index) {
+              return each;
+            }
+          });
+          setImage(images);
+        },
+      },
+    ]);
+  }
+  console.log("image array : ",image);
   return (
     <View style={styles.container}>
       <BottomSheet ref={bottomSheet} height={350}>
@@ -21,14 +68,15 @@ const App = () => {
           <Text style={styles.header}>Upload</Text>
         </View>
         <View style={styles.horizontaline}></View>
-        <View
+        <TouchableOpacity
+          onPress={usingCamera}
           style={[
             styles.flexcontainer,
             {borderBottomWidth: 1, borderBottomColor: '#e5e5e5'},
           ]}>
           <Feather name="camera" size={25} color={'black'} />
           <Text style={styles.text}>Camera</Text>
-        </View>
+        </TouchableOpacity>
         <View
           style={[
             styles.flexcontainer,
@@ -37,14 +85,15 @@ const App = () => {
           <Ionicons name="document-text-outline" size={25} color={'black'} />
           <Text style={styles.text}>Documents</Text>
         </View>
-        <View
+        <TouchableOpacity
+          onPress={fromGallery}
           style={[
             styles.flexcontainer,
             {borderBottomWidth: 1, borderBottomColor: '#e5e5e5'},
           ]}>
           <Ionicons name="images-outline" size={25} color={'black'} />
           <Text style={styles.text}>Gallery</Text>
-        </View>
+        </TouchableOpacity>
       </BottomSheet>
       <TouchableOpacity
         style={styles.iconcontainer}
@@ -57,6 +106,36 @@ const App = () => {
         their obligations prescribed in the terms of Use governed under the
         Personal Data Protection Act
       </Text>
+      <Text style={styles.text1}>Uploaded images</Text>
+      <View style={styles.imagecontainer}>
+        {image.length > 0 &&
+          image.map((each, index) => {
+            return (
+              <View key={index} style={styles.image}>
+                <Image
+                  source={{uri: each.path}}
+                  style={{
+                    height: 80,
+                    width: 80,
+                    borderRadius: 15,
+                  }}
+                  key={index}
+                />
+                <TouchableOpacity onPress={()=>{deleteSingleImage(index)}}>
+                  <FontAwesome name="close" size={20} color={'#ffc30b'} style={styles.icon} />
+                </TouchableOpacity>
+              </View>
+            );
+          })}
+      </View>
+      <View style={styles.buttoncontainer}>
+        <TouchableOpacity style={styles.buttonoutline}>
+          <Text style={styles.buttonoutlinetext}>Reset</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttontext}>Submit</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -83,6 +162,12 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     marginLeft: 15,
   },
+  text1: {
+    marginLeft: -150,
+    color: 'black',
+    fontSize: 15,
+    marginTop: 20,
+  },
   smalltext: {
     color: 'gray',
     fontSize: 15,
@@ -91,14 +176,21 @@ const styles = StyleSheet.create({
   headercontainer: {
     display: 'flex',
     flexDirection: 'row',
-    paddingVertical:25,
-    marginLeft:10
+    paddingVertical: 25,
+    marginLeft: 10,
   },
   flexcontainer: {
     display: 'flex',
     flexDirection: 'row',
     marginTop: 20,
-    marginLeft:10
+    marginLeft: 10,
+  },
+  buttoncontainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    columnGap: 20,
+    position: 'absolute',
+    bottom: 15,
   },
   horizontaline: {
     borderBottomColor: 'skyblue',
@@ -107,7 +199,51 @@ const styles = StyleSheet.create({
   header: {
     fontSize: 20,
     color: 'black',
-    marginLeft:10,
-    marginTop:-5
+    marginLeft: 10,
+    marginTop: -5,
   },
+  button: {
+    width: 150,
+    padding: 10,
+    backgroundColor: '#ffc30b',
+    borderRadius: 15,
+  },
+  buttontext: {
+    textAlign: 'center',
+    fontSize: 15,
+    color: 'white',
+  },
+  buttonoutline: {
+    width: 150,
+    padding: 10,
+    borderWidth: 2,
+    borderColor: '#ffc30b',
+    borderRadius: 15,
+  },
+  buttonoutlinetext: {
+    textAlign: 'center',
+    fontSize: 15,
+    color: '#ffc30b',
+  },
+  imagecontainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    columnGap: 5,
+    paddingTop: 10,
+  },
+  image:{
+    height:110,
+    width:110,
+    elevation:5,
+    backgroundColor:"white",
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  icon:{
+    position:"absolute",
+    top:-95,
+    left:33
+  }
 });
